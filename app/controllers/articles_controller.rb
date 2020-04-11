efa196bf8d1b0726e02f2ad3972aac2d698679d0
Maximilian Schmitt <max@schmitt-wtw.de>
2020-04-11 09:24:14 +0200

    Add show view styling, style validation and flash messages and clean up layout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfully"
+      flash[:success] = "Article was successfully created"
       redirect_to @article
     else 
       render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   def update 
     if @article.update(article_params)
-      flash[:notice] = "Article was updated successfully"
+      flash[:success] = "Article was successfully updated"
       redirect_to @article
     else
       render 'edit'
@@ -37,6 +37,7 @@ class ArticlesController < ApplicationController
 
   def destroy 
     @article.destroy
+    flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
 
